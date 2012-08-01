2a95febc1d0999400f688ccdd547a24cd6ea1d4a
tnantoka <bornneet@livedoor.com>
2012-08-01 23:40:38 +0900

    Change Bye, World

---
+++ HelloObjectiveC/HelloObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by Tatsuya Tobioka on 12/08/01.
+//  Copyright (c) 2012年 tnantoka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Bye, World!");
+        
+    }
+    return 0;
+}
+
